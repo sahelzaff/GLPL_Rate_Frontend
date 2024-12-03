745fd301e43beaab66c6ff29bb1c81c000e885e4
Sahel Zaffarulla <sahel@goodrichlogistics.local>
2024-12-04 01:40:58 +0530

    initial Commit

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb+srv://goodrichlogisticsmobile:1HEKHz1CM56clIDE@cluster0.7a6zw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
