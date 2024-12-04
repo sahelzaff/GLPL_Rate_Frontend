c594d10f4b049fef6946932d23bd954f22244313
Sahel Zaffarulla <sahel@goodrichlogistics.local>
2024-12-04 17:33:16 +0530

    production

--- src/app/results/page.jsx
+++ src/app/results/page.jsx
@@ -558,7 +558,7 @@ function ResultsContent() {
             setLoading(true);
             setError(null);
 
-            const response = await fetch('http://localhost:5001/api/rates/search', {
+            const response = await fetch('https://glplratebackend-production.up.railway.app//api/rates/search', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
